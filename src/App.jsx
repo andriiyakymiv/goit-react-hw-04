7423e27936d1553abd9dc94386395d96f82b328e
andrii <a.iakumiv@gmail.com>
2025-04-23 10:44:13 +0300

    Added component structure (SearchBar) and logic behavior

--- src/App.jsx
+++ src/App.jsx
@@ -1,71 +1,95 @@
 import { useEffect, useState } from "react";
 
-import { getPhotos } from "./service/unsplashApi";
 
 
-import SearchBar from './components/SearchBar/SearchBar';
 
 
-import './App.css'
+import { getPhotos } from "./services/unsplashApi";
+import SearchBar from "./components/SearchBar/SearchBar";
+
+
 function App() {
   const [query, setQuery] = useState("");
   const [page, setPage] = useState(1);
-  const [images, setImages] = useState([]);
-  const [isloading, setIsLoading] = useState(false);
+  const [pictures, setPictures] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(false);
-  const [showLoadMore, setShowLoadMore] = useState(false);
+  const [totalPages, setTotalPages] = useState(0);
+  //add a check on invalid query
   const [isEmpty, setIsEmpty] = useState(false);
+  //modal
   const [modalImage, setModalImage] = useState(null);
 
   useEffect(() => {
-    if (!query) return;
-
-    const fetchData = async () => {
+    if (!query) {
+      return;
+    }
+    const fetchImages = async () => {
       setIsLoading(true);
       try {
-        const results = await getPhotos(query, page);
-        if (results.length === 0) {
+        const { results, totalPages } = await getPhotos(query, page);
+        setPictures((prev) => {
+          return [...prev, ...results];
+        });
+        if (!totalPages) {
           setIsEmpty(true);
-          setShowLoadMore(false);
-        } else {
-          setIsEmpty(false);
-          setImages((prevImages) => [...prevImages, ...results]);
-          setShowLoadMore(true);
+          return;
         }
+        setTotalPages(totalPages);
       } catch (error) {
         setError(error.message);
       } finally {
         setIsLoading(false);
       }
     };
-
-    fetchData();
+    fetchImages();
   }, [query, page]);
 
   const handleSubmit = (query) => {
-    setQuery(query);
-    setPage(1);
-    setImages([]);
-    setError("");
-    setShowLoadMore(false);
     setIsEmpty(false);
+    setQuery(query);
+    setPictures([]);
+    setPage(1);
+    setError("");
+    setTotalPages(1);
   };
-
-  const openModal = (img) => {
-    setModalImage(img);
-  };
-
-  const handleClick = () => {
+  const handleLoadMore = () => {
     setPage((prevPage) => prevPage + 1);
+  };
+
+  //modal
+  const openModal = (image) => {
+    setModalImage(image);
   };
 
   return (
     <>
-      <div>
-        <SearchBar onSubmit={handleSubmit} />
-      </div>
+      <SearchBar onSubmit={handleSubmit} />
+
+      {/* {pictures.length > 0 && (
+        <ImageGallery openModal={openModal} pictures={pictures} />
+      )} */}
+
+      {/* {error && <ErrorMessage message={error} />} */}
+
+      {/* {isLoading && <Loader />} */}
+
+      {/* {isEmpty && "Please enter a valid search query"} */}
+
+      {/* {pictures.length > 0 && page < totalPages && (
+        <LoadMoreButton onClick={handleLoadMore} />
+      )} */}
+
+      {/* <ModalImage
+        picture={modalImage}
+        modalIsOpen={Boolean(modalImage)}
+        closeModal={() => {
+          openModal(null);
+        }}
+      /> */}
+
     </>
   );
-};
+}
 
 export default App;
