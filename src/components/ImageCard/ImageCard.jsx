9725436d7c760b066d1a4509ad48222a574c2a1f
andrii <a.iakumiv@gmail.com>
2025-04-23 11:30:38 +0300

    Added component structure (ImageCard) (ImageGallery) as well as styling

---
+++ src/components/ImageCard/ImageCard.jsx
@@ -0,0 +1,25 @@
+import Description from "../Description/Description";
+
+import style from "./ImageCard.module.css";
+
+const ImageCard = ({ picture, openModal }) => {
+  return (
+    <li>
+      <div className={style.imageWrapper}>
+        <img
+          onClick={() => {
+            openModal({
+              src: picture.urls.regular,
+              alt: picture.alt_description,
+            });
+          }}
+          src={picture.urls.small}
+          alt={picture.alt_description}
+        />
+        <Description picture={picture} />
+      </div>
+    </li>
+  );
+};
+
+export default ImageCard;
--- src/services/unsplashApi.js
+++ src/services/unsplashApi.js
@@ -1,13 +1,11 @@
 import axios from "axios";
-
 const ACCESS_KEY = "_p6UAMB0X3kVJoMRh10BP_zHt3Rb4eXXKJy3p0OEsps";
-
 const instance = axios.create({
   baseURL: "https://api.unsplash.com/",
   params: {
     client_id: ACCESS_KEY,
     orientation: "landscape",
-    per_page: "9",
+    per_page: "12",
   },
 });
 
@@ -15,6 +13,8 @@ export const getPhotos = async (query, page) => {
   const { data } = await instance.get(`search/photos`, {
     params: { query, page },
   });
-
-  return data.results;
+  return {
+    results: data.results,
+    totalPages: data.total_pages,
+  };
 };
